0d41a5201976d3324ba9131b4b97b075530df247
karolherbst <git@karolherbst.de>
2013-01-14 11:37:30 +0100

    make openPath and closedPath protected
    this will allow additional drawing like alpha masking or other overlaid
    stuff

--- src/org/sexydock/tabs/jhrome/JhromeTabBorder.java
+++ src/org/sexydock/tabs/jhrome/JhromeTabBorder.java
@@ -41,8 +41,8 @@ public class JhromeTabBorder implements Border
 {
 	public final JhromeTabBorderAttributes	attrs	= new JhromeTabBorderAttributes( );
 	
-	private Path2D							openPath;
-	private Path2D							closedPath;
+	protected Path2D							openPath;
+	protected Path2D							closedPath;
 	
 	private void updatePaths( int x , int y , int width , int height )
 	{
